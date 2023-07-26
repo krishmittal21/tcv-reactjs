47fdcc998f2f2d1feddd54102460642c517515e4
Krish Mittal <114004042+krishmittal21@users.noreply.github.com>
2023-07-26 18:50:36 +0530

    cleanup

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,47 +1,31 @@
 import bg from '../assets/bg.png';
-import chef from '../assets/chef.png';
 import gallery01 from '../assets/08e1a96a9dfd53e0635b66777844f69e.jpg';
 import gallery02 from '../assets/welcome.jpg';
 import gallery03 from '../assets/Oreo-Cake-Square-2-500x500.webp';
 import gallery04 from '../assets/istockphoto-899069970-612x612.jpg';
 import knife from '../assets/knife.png';
-import logo from '../assets/logo.png';
 import menu from '../assets/menu.png';
 import overlaybg from '../assets/overlaybg.png';
 import spoon from '../assets/spoon.svg';
 import welcome from '../assets/welcome.jpg';
 import findus from '../assets/findus.png';
-import laurels from '../assets/laurels.png';
-import award01 from '../assets/award01.png';
-import award02 from '../assets/award02.png';
-import award03 from '../assets/award03.png';
-import award05 from '../assets/award05.png';
-import sign from '../assets/sign.png';
 import quote from '../assets/quote.png';
 import tcv from '../assets/tcv.png';
 import t from '../assets/t-removebg-preview.png';
 import candle from '../assets/candle.png';
 export default {
   bg,
-  chef,
   t,
   gallery01,
   gallery02,
   gallery03,
   gallery04,
   knife,
-  logo,
   menu,
   overlaybg,
   spoon,
   welcome,
   findus,
-  laurels,
-  award01,
-  award02,
-  award03,
-  award05,
-  sign,
   quote,
   tcv,
   candle,
